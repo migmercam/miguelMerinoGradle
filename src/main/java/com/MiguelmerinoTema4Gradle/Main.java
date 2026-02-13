e46ff76037f504956f62d3f6059617d9394057a7
migmercam <memiguelsmx@gmail.com>
2026-02-13 13:30:53 +0100

    Añadida la segunda ia, y solucionado lo de los simbolos

--- src/main/java/com/MiguelmerinoTema4Gradle/Main.java
+++ src/main/java/com/MiguelmerinoTema4Gradle/Main.java
@@ -5,6 +5,8 @@ import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,24 +15,37 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         //No me funciona el ponerlo desde setings ni desde la esquina, asique me ha tocado forzarlo
-        System.setProperty("file.encoding", "UTF-8");
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
         // El TOKEN no es necesario para interactuar con modelos locales
         final String TOKEN = "PEGA_AQUI_TU_TOKEN";
 
-        var model = OpenAiChatModel.builder()
+        var llama = OpenAiChatModel.builder()
                 .baseUrl("http://localhost:11434/v1")
                 .apiKey(TOKEN)
                 .modelName("llama3.1:8b")
                 .build();
+
+        var gemma = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("gemma:2b")
+                .build();
+
         List<ChatMessage> history = new ArrayList<>();
+//Ponemos un try/catch para que en caso de error de conexion nos salte el mensaje en vez de la parrafada
+        try {
         // Interacción 1
         history.add(new UserMessage("Hola, soy Miguel"));
-        AiMessage respuesta = model.chat(history).aiMessage();
+        AiMessage respuesta = llama.chat(history).aiMessage();
         history.add(respuesta);
         // Interacción 2
         history.add(new UserMessage("¿Recuerdas cómo me llamo?"));
-        respuesta = model.chat(history).aiMessage();
+        respuesta = llama.chat(history).aiMessage();
         history.add(respuesta);
         System.out.println(respuesta.text());
+
+        } catch (Exception e) {
+            System.err.println("Error: No se ha podido conectar con el servidor de Ollama. Asegúrate de que esta activo.");
+        }
     }
 }
