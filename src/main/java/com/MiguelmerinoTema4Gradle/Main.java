8aacd0679f4cbaf7e56100a2801b8f95fa9bb055
migmercam <memiguelsmx@gmail.com>
2026-02-13 11:36:49 +0100

    Instalados los modelos gemma:2b yllama3.1:8b y pegado el codigo de comprobacion, sin testear

--- src/main/java/com/MiguelmerinoTema4Gradle/Main.java
+++ src/main/java/com/MiguelmerinoTema4Gradle/Main.java
@@ -1,9 +1,20 @@
 package com.MiguelmerinoTema4Gradle;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // El TOKEN no es necesario para interactuar con modelos locales
+        final String TOKEN = "PEGA_AQUI_TU_TOKEN";
+
+        var model = OpenAiChatModel.builder()
+                .baseUrl("https://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("gemma:2b")
+                .build();
+        String respuesta = model.chat("Cuéntame un chiste");
 
     }
 }
